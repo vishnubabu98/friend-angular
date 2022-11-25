506928cbffe8614f071968a6e1f2e76c76fa9a58
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-25 19:40:03 +0530

    add viewtable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddFriendComponent } from './add-friend/add-friend.component';
+import { ViewFriendComponent } from './view-friend/view-friend.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddFriendComponent
+    AddFriendComponent,
+    ViewFriendComponent
   ],
   imports: [
     BrowserModule,
